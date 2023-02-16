36fce426f0fb58eb958e95d3f730cf9a2ea55374
Muhammad <mohkt@yahoo.com>
2023-02-16 09:36:52 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include<unistd.h>
 /**
  * main - program that prints and that piece of art is useful
- * Return 1 (success)
+ * Return: 1 sucess
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
