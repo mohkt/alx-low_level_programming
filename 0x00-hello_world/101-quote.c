3824c7b9874490a4998db7c817e2019864bd494e
Muhammad <mohkt@yahoo.com>
2023-02-16 09:18:34 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main - program that prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - program that prints and that piece of art is useful
  * Return 1 (success)
  */
 int main(void)
